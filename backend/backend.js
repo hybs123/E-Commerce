e33f7603ccfc51dd49ad81213c1f24c89aa1f2a3
hybs123 <haidersoni47@gmail.com>
2024-09-23 17:41:47 +0530

    backlocal

--- backend/backend.js
+++ backend/backend.js
@@ -54,7 +54,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: "https://localhost:5173",
+    origin: "http://localhost:5173/",
     // origin: "https://sara-organics.onrender.com",
     credentials: true,
   })
