44e184c93220be035029f6f45520daceaf0bbde3
hybs123 <haidersoni47@gmail.com>
2024-09-25 15:01:45 +0530

    bas

--- backend/backend.js
+++ backend/backend.js
@@ -54,13 +54,7 @@ app.use(
     // Other session configuration options
   })
 );
-app.get("*", (req, res) => {
-  try {
-      res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-  } catch (e) {
-      res.send("Welcome to Sara Organics");
-  }
-});
+
 
 app.use(
   cors({
@@ -126,7 +120,13 @@ app.use(passport.initialize());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+app.get("*", (req, res) => {
+  try {
+      res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  } catch (e) {
+      res.send("Welcome to Sara Organics");
+  }
+});
 app.get("/", (req, res) => {
   return res.send("<h1>Hello</h1>");
 });
